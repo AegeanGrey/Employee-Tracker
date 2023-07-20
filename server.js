4272bbe385b9a86451894d815cba0a9dc3edfd93
Aegean <todddharni@gmail.com>
2023-07-20 14:17:55 -0600

    Moved contents from index.js to server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // Takes the dependencies needed from package.json and stores them within variables
+const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
 
-// Takes the CLI Class from index.js and creates a new instance of it
-const CLI = require('./lib/index.js');
-const cli = new CLI();
+// Allows us to use options and question arrays from 'cli.js'
+const { options, question } = require('./lib/cli.js');
 
 // Establishes express library call
 const app = express();
@@ -24,18 +24,82 @@ const db = mysql.createConnection(
   console.log(`Connection successful`)
 );
 
+class CLI {
+  constructor () {}
+  run() {
+    return inquirer
+  
+    // Prompts user with question variable from 'cli.js'
+    .prompt(question)
+  
+    // Takes user response stored in 'userSelect'
+    .then(({ userSelect }) => {
+
+    // Switch case that compares 'userSelect' with the 'options' array to find a match
+    switch (userSelect) {
+      
+        // Once a match is found it will execute a line of code that the matched statement is associated with
+        case options[0]:
+          db.query('SELECT name FROM department', (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(result);
+            cli.run();
+          }
+        });
+        break;
+              
+        case options[1]:
+          console.log('Viewing all Roles');
+        break;
+                  
+        case options[2]:
+          console.log('Viewing all Employees');
+        break;
+  
+        case options[3]:
+          console.log('Adding a new Department');
+        break;
+              
+        case options[4]:
+          console.log('Adding a new Role');
+        break;
+                  
+        case options[5]:
+          console.log('Adding a new Employee');
+        break;
+                      
+        case options[6]:
+          console.log('Updating an Employee');
+        break;
+      
+        case options[7]:
+          console.log('Exiting Application');
+        break;
+        }
+    });           
+  }
+}
+
+// Takes the CLI Class and creates a new instance of it
+const cli = new CLI();
+
 // Calls the run function within the CLI class
 cli.run();
 
 /*
 // Shows the department table contents in the console
-db.query('SELECT * FROM department', (err, result) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
-});
+function viewDepartments() {
+  db.query('SELECT * FROM department', (err, result) => {
+      if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+}
+
 
 // Shows the role table contents in the console
 db.query('SELECT * FROM role', (err, result) => {
